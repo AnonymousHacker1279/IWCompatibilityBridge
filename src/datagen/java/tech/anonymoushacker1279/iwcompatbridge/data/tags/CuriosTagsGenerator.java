aeb07aaef679e1a785ea877c33f6c21043899736
Nicholas Smit <48810167+AnonymousHacker1279@users.noreply.github.com>
2026-08-30 19:03:38 -0400

    Port to MC 26.2
    Port to MC 26.2 and IW v1.39.0 and prepare for release.

--- src/datagen/java/tech/anonymoushacker1279/iwcompatbridge/data/tags/CuriosTagsGenerator.java
+++ src/datagen/java/tech/anonymoushacker1279/iwcompatbridge/data/tags/CuriosTagsGenerator.java
@@ -3,7 +3,7 @@ package tech.anonymoushacker1279.iwcompatbridge.data.tags;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
+import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -12,7 +12,7 @@ import tech.anonymoushacker1279.iwcompatbridge.IWCompatBridge;
 
 import java.util.concurrent.CompletableFuture;
 
-public class CuriosTagsGenerator extends IntrinsicHolderTagsProvider<Item> {
+public class CuriosTagsGenerator extends TagsProvider<Item> {
 
 	public static final TagKey<Item> BELT = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("curios", "belt"));
 	public static final TagKey<Item> BODY = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("curios", "body"));
@@ -25,64 +25,65 @@ public class CuriosTagsGenerator extends IntrinsicHolderTagsProvider<Item> {
 	public static final TagKey<Item> SPIRIT = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("curios", "spirit"));
 
 	public CuriosTagsGenerator(PackOutput output, CompletableFuture<Provider> lookupProvider) {
-		super(output, Registries.ITEM, lookupProvider, item -> item.builtInRegistryHolder().key(), IWCompatBridge.MOD_ID);
+		super(output, Registries.ITEM, lookupProvider, IWCompatBridge.MOD_ID);
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	protected void addTags(Provider provider) {
 		tag(BELT).add(
-				ItemRegistry.SATCHEL.get(),
-				ItemRegistry.HOLY_MANTLE.get(),
-				ItemRegistry.VENSTRAL_JAR.get());
+				ItemRegistry.SATCHEL.getKey(),
+				ItemRegistry.HOLY_MANTLE.getKey(),
+				ItemRegistry.VENSTRAL_JAR.getKey());
 
 		tag(BODY).add(
-				ItemRegistry.POWDER_HORN.get(),
-				ItemRegistry.BLOATED_HEART.get(),
-				ItemRegistry.NETHERITE_SHIELD.get(),
-				ItemRegistry.SUPER_BLANKET_CAPE.get());
+				ItemRegistry.POWDER_HORN.getKey(),
+				ItemRegistry.BLOATED_HEART.getKey(),
+				ItemRegistry.NETHERITE_SHIELD.getKey(),
+				ItemRegistry.SUPER_BLANKET_CAPE.getKey());
 
-		tag(BRACELET).add(ItemRegistry.AGILITY_BRACELET.get());
+		tag(BRACELET).add(ItemRegistry.AGILITY_BRACELET.getKey());
 
 		tag(CHARM).add(
-				ItemRegistry.BERSERKERS_AMULET.get(),
-				ItemRegistry.BLADEMASTER_EMBLEM.get(),
-				ItemRegistry.DEPTH_CHARM.get(),
-				ItemRegistry.REINFORCED_DEPTH_CHARM.get(),
-				ItemRegistry.ANCIENT_SCROLL.get(),
-				ItemRegistry.MEDAL_OF_HONOR.get(),
-				ItemRegistry.MEDAL_OF_DISHONOR.get());
+				ItemRegistry.BERSERKERS_AMULET.getKey(),
+				ItemRegistry.BLADEMASTER_EMBLEM.getKey(),
+				ItemRegistry.DEPTH_CHARM.getKey(),
+				ItemRegistry.REINFORCED_DEPTH_CHARM.getKey(),
+				ItemRegistry.ANCIENT_SCROLL.getKey(),
+				ItemRegistry.MEDAL_OF_HONOR.getKey(),
+				ItemRegistry.MEDAL_OF_DISHONOR.getKey());
 
 		tag(HANDS).add(
-				ItemRegistry.MELEE_MASTERS_MOLTEN_GLOVE.get(),
-				ItemRegistry.IRON_FIST.get(),
-				ItemRegistry.GLOVE_OF_RAPID_SWINGING.get(),
-				ItemRegistry.HAND_OF_DOOM.get());
+				ItemRegistry.MELEE_MASTERS_MOLTEN_GLOVE.getKey(),
+				ItemRegistry.IRON_FIST.getKey(),
+				ItemRegistry.GLOVE_OF_RAPID_SWINGING.getKey(),
+				ItemRegistry.HAND_OF_DOOM.getKey());
 
 		tag(HEAD).add(
-				ItemRegistry.GOGGLES.get(),
-				ItemRegistry.LAVA_GOGGLES.get(),
-				ItemRegistry.NIGHT_VISION_GOGGLES.get(),
-				ItemRegistry.BLOODY_CLOTH.get());
+				ItemRegistry.GOGGLES.getKey(),
+				ItemRegistry.LAVA_GOGGLES.getKey(),
+				ItemRegistry.NIGHT_VISION_GOGGLES.getKey(),
+				ItemRegistry.BLOODY_CLOTH.getKey());
 
 		tag(NECKLACE).add(
-				ItemRegistry.DEADEYE_PENDANT.get(),
-				ItemRegistry.MEDAL_OF_ADEQUACY.get(),
-				ItemRegistry.INSOMNIA_AMULET.get());
+				ItemRegistry.DEADEYE_PENDANT.getKey(),
+				ItemRegistry.MEDAL_OF_ADEQUACY.getKey(),
+				ItemRegistry.INSOMNIA_AMULET.getKey());
 
 		tag(RING).add(
-				ItemRegistry.COPPER_RING.get(),
-				ItemRegistry.IRON_RING.get(),
-				ItemRegistry.COBALT_RING.get(),
-				ItemRegistry.GOLDEN_RING.get(),
-				ItemRegistry.AMETHYST_RING.get(),
-				ItemRegistry.EMERALD_RING.get(),
-				ItemRegistry.DIAMOND_RING.get(),
-				ItemRegistry.NETHERITE_RING.get(),
-				ItemRegistry.DEATH_GEM_RING.get());
+				ItemRegistry.COPPER_RING.getKey(),
+				ItemRegistry.IRON_RING.getKey(),
+				ItemRegistry.COBALT_RING.getKey(),
+				ItemRegistry.GOLDEN_RING.getKey(),
+				ItemRegistry.AMETHYST_RING.getKey(),
+				ItemRegistry.EMERALD_RING.getKey(),
+				ItemRegistry.DIAMOND_RING.getKey(),
+				ItemRegistry.NETHERITE_RING.getKey(),
+				ItemRegistry.DEATH_GEM_RING.getKey());
 
 		tag(SPIRIT).add(
-				ItemRegistry.HANS_BLESSING.get(),
-				ItemRegistry.CELESTIAL_SPIRIT.get(),
-				ItemRegistry.VOID_BLESSING.get());
+				ItemRegistry.HANS_BLESSING.getKey(),
+				ItemRegistry.CELESTIAL_SPIRIT.getKey(),
+				ItemRegistry.VOID_BLESSING.getKey());
 	}
 }
